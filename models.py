f2813fc02d3442edbfbf7eb918676f8d4c90d181
tdogubo <tdogubo@gmail.com>
2021-03-25 20:48:18 +0100

    review needed

--- models.py
+++ models.py
@@ -2,24 +2,36 @@ from marshmallow import fields
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.schema import FetchedValue
 from uuid import uuid4
 
 ma = Marshmallow()
 db = SQLAlchemy()
+
+class Likes(db.Model):
+    __tablename__= 'likes'
+    tweet_id =db.Column(UUID(as_uuid=True), db.ForeignKey('tweets.id', ondelete='CASCADE'),primary_key=True, nullable=False, default = lambda: uuid4().hex,unique = False)
+    likes =db.Column(UUID(as_uuid=True), db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False, default = lambda: uuid4().hex)
+    creation_date = db.Column(db.TIMESTAMP, server_default= db.func.now(), nullable = False)
+    user = db.relationship('User', backref= db.backref('likes', lazy='dynamic'))
+    tweets = db.relationship('Tweet', backref= db.backref('likes', lazy='dynamic'))
+
+
+    def __init__(self,tweet_id,likes):
+        self.likes= likes
+        self.tweet_id= tweet_id
 
 class Tweet(db.Model):
     __tablename__= 'tweets'
     id = db.Column(UUID(as_uuid=True), primary_key = True,default = lambda: uuid4().hex)
     tweet = db.Column(db.String(500), nullable = False)
     creation_date = db.Column(db.TIMESTAMP, server_default= db.func.now(), nullable = False)
-    likes = db.Column(UUID(as_uuid=True),default=0)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False, default = lambda: uuid4().hex)
     user = db.relationship('User', backref= db.backref('tweets', lazy='dynamic'))
 
-    def __init__(self,tweet,user_id,likes):
+    def __init__(self,tweet,user_id):
         self.tweet= tweet
         self.user_id= user_id
-        self.likes = likes
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -54,6 +66,10 @@ class UserSchema(ma.Schema):
 class TweetSchema(ma.Schema):
     id= fields.UUID(as_uuid=True)
     user_id=fields.UUID(required= True)
-    likes=fields.UUID(as_uuid= True)
     tweet= fields.String(required=True)
+    creation_date = fields.DateTime()
+
+class LikesSchema(ma.Schema):
+    tweet_id= fields.UUID(required=True,unique=False)
+    likes=fields.UUID(required= True)
     creation_date = fields.DateTime()
