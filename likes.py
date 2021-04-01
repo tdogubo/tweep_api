5b9837eef7ca3e946b6db21f4a28bc317cbb431b
tdogubo <tdogubo@gmail.com>
2021-04-01 20:22:31 +0100

    likes and models modified

--- likes.py
+++ likes.py
@@ -1,40 +1,40 @@
-from flask import Blueprint, request,abort
+from flask import Blueprint, request, abort
 from flask_restful import Resource
-from models import db,Likes,LikesSchema
+from models import db,Tweet,User
 from tweets import tweets_api
 
-likes_blueprint = Blueprint('likes',__name__)
 
-like_schema = LikesSchema()
-likes_schema = LikesSchema(many=True)
+
+likes_blueprint = Blueprint('likes',__name__)
 
 
 class Like(Resource):
     def get(self,tweet_id):
-        tweet = Likes.query.filter_by(tweet_id=tweet_id)
-        tweet = likes_schema.dump(tweet)
-        count = len(tweet)
-        return {'tweet':tweet,'likes: ':count}
+        x = User.query.filter(User.tweets.any(id=tweet_id)).all()
+        return {'message': x}
+
     
     def post(self,tweet_id):
         json_data = request.get_json(force= True)
         if not request.is_json:
             return {'message': 'invalid input'}
-        user_tweet= json_data['tweet_id']
-        like = json_data['user_id']
+        user_id = json_data['user_id']
+        user= User.query.filter_by(id=user_id).first()
+        tweet= Tweet.query.filter_by(id=tweet_id).first()
 
-        tweet= Likes.query.filter_by(tweet_id=tweet_id).first()
-        like= Likes.query.filter_by(likes=like).first()
-
-        if tweet and like:
-            return {'message':'tweet already liked'}, 200
-            
-        likes= Likes(likes= like,tweet_id=user_tweet)
-        db.session.add(likes)
+        user.tweets.append(tweet)
+        db.session.add(tweet)
         db.session.commit()
+        return 'done'
 
-        result=like_schema.dump(likes)
-        return result, 200
+    def delete(self,tweet_id):
+        user= db.session.query(User).get(tweet_id)
 
+        if not user:
+            abort(404, "User not found")
+
+        db.session.delete(user)
+        db.session.commit()
+        return 'done'
 
 tweets_api.add_resource(Like,'/tweet/<string:tweet_id>')
